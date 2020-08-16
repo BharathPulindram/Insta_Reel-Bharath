72bbae52769dd812bebd26771acf3c0d8ef828ba
Bharath Kumar <pbkbharath1707@gmail.com>
2020-08-16 12:01:20 +0530

    Updates

---
+++ precache-manifest.69f12efba623bf794d7073bc5d9c84a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6343ae71b8479c77888229eea8ad7d98",
+    "url": "/Insta_Reel-Bharath/index.html"
+  },
+  {
+    "revision": "b642a507ef055c16e92a",
+    "url": "/Insta_Reel-Bharath/static/css/main.26044046.chunk.css"
+  },
+  {
+    "revision": "c3c79946ea54804d621a",
+    "url": "/Insta_Reel-Bharath/static/js/2.9aeb8965.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Insta_Reel-Bharath/static/js/2.9aeb8965.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b642a507ef055c16e92a",
+    "url": "/Insta_Reel-Bharath/static/js/main.a2d0832f.chunk.js"
+  },
+  {
+    "revision": "97401c934b712dc580b2",
+    "url": "/Insta_Reel-Bharath/static/js/runtime-main.0363803b.js"
+  }
+]);
